b2201408d03ff204231395ae305c76fa67ee5370
Jasper Barcelona <barcelona.jasperoliver@gmail.com>
2018-11-21 00:53:24 +0800

    a

--- main/static/js/login_animations.js
+++ main/static/js/login_animations.js
@@ -1,14 +1,10 @@
-$(document).ready(function(){
+f$(document).ready(function(){
 
 $('#login-error-container').hide();
 
 $(window).load(function() {
 	$("#user_email").focus();
     $('#login-intro').hide();
-});
-
-$('#login-form .form-control').floatlabel({
-    labelEndTop:'-2px'
 });
 
 $('#login-form').on('submit', function(e){
